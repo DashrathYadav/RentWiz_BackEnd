4dd3017fc4a260278712cbf886e5220606e8c4ce
zeus-anuj <panditanuj193@gmail.com>
2024-08-24 21:25:43 +0530

    Add colon

--- models/Room.js
+++ models/Room.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Room extends Model {
     /**
-     * Helper method for defining associations.
+     * Helper method for defining associations.:
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
